cce2cb2a80096dfbea456708b878f91fa65b2855
skydanx1 <61358204+skydanx1@users.noreply.github.com>
2020-02-22 11:16:47 -0600

    Update Persona.java

--- 1-Humano/src/pkg/humano/Persona.java
+++ 1-Humano/src/pkg/humano/Persona.java
@@ -27,7 +27,7 @@ public class Persona {
         this.nombre = nombre;
         this.edad = edad;
         this.sexo = sexo;
-        // Roberto Gay
+        // Roberto COPION!!!
     }
 
     public String getNombre() {
